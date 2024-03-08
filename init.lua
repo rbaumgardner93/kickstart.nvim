d3095a27e5bd4ac952ee6e48a8ab01e1c4f7deec
Ryan Baumgardner <rbaumgardner@planview.com>
2024-03-08 15:55:50 -0500

    feat: update a few default options

--- init.lua
+++ init.lua
@@ -90,8 +90,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -99,10 +99,10 @@ vim.g.have_nerd_font = false
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
+-- vim.opt.number = true
+-- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -160,9 +160,6 @@ vim.opt.scrolloff = 10
 -- [[ Custom Setting options ]]
 -- Show character count with line
 vim.opt.colorcolumn = '80'
-
--- Show relative line numbers
-vim.opt.relativenumber = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
