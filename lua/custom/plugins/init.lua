713a24dcd40f87ed16df592014ba49f608f3b7a4
Ryan Baumgardner <rbaumgardner@planview.com>
2024-03-08 08:53:10 -0500

    feat: add vim-vinegar and vim-fugitive

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'tpope/vim-vinegar',
+  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gs', '<cmd>G<CR>', { desc = '[G]it [S]tatus' })
+    end,
+  },
+}
